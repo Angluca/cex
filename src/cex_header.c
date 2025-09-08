78f43382287b183896aafb8936ad6f0de5ac5a34
Alex Veden <i@alexveden.com>
2025-09-08 11:47:46 +0400

    fix: added alias for dup/dup2/fileno on windows

--- src/cex_header.c
+++ src/cex_header.c
@@ -5,6 +5,11 @@
 #    define WIN32_LEAN_AND_MEAN
 #    include "windows.h"
 #    include <direct.h>
+#    include <io.h>
+#    define close _close
+#    define dup _dup
+#    define dup2 _dup2
+#    define fileno _fileno
 #else
 #    include <dirent.h>
 #    include <fcntl.h>
